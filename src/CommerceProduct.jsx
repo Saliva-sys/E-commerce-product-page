6f570f921e2d7ae6e71c612d5ddf3decdd87218e
Saliva-sys <aweidlichova@gmail.com>
2026-03-26 21:10:09 +0100

    fix: export default component

---
+++ src/CommerceProduct.jsx
@@ -0,0 +1,10 @@
+const CommerceProduct = () => {
+  return (
+    <div>
+      <h1>E-Commerce Product Page</h1>
+      <p>Príprava úspešná, zajtra pokračujeme!</p>
+    </div>
+  );
+};
+
+export default CommerceProduct;
