1aacf23fee41364f60a89db6821fdc782e845952
Saliva-sys <aweidlichova@gmail.com>
2026-03-31 19:31:16 +0200

    refactor: restructure data loading and fix state logic for cart

--- src/CommerceProduct.jsx
+++ src/CommerceProduct.jsx
@@ -1,190 +1,215 @@
 import "./CommerceProduct.css";
 import  {useState, useEffect} from "react";
+import Close from "./assets/icon-close.svg";
 import Menu from "./assets/icon-menu.svg";
 import Logo from "./assets/logo.svg";
 import Cart from "./assets/icon-cart.svg";
-import Bascket from "./assets/icon-delete.svg";
-import Close from "./assets/icon-close.svg";
 import Avatar from "./assets/image-avatar.png";
-import Plus from "./assets/icon-plus.svg";
 import Minus from "./assets/icon-minus.svg";
+import Plus from "./assets/icon-plus.svg";
 
-
-const CommerceProduct = () => { // idem vyberat produkty a vkladat ich cez button "Add to Cart" do kosika
+// ==========================================
+// Stav a logika
+const CommerceProduct = () => {
+  const [menu, setMenu] = useState(false); // vyber "menu"
+  const [cart, setCart] = useState(false); // otvorenie kosika z navigacneho panela
   const [products, setProducts] = useState([]);
+  const [productImages, setProductImages] = useState(); //vyber obrazkov z thumbnail
+  const [quantity, setQuantity] = useState(0)
   const [addCart, setAddCart] = useState(false);
-  const [quantity, setQuantity] = useState(1);
+// ==========================================
   
-  // idem vyberat z thumbnails obrazky
-  const [productImages,setProductImages] = useState();
 
-  const unitPrice = products[0]?.price || 0; // Cena za jeden kus
-  const totalPrice = unitPrice * quantity; // Celková cena sa vypočíta sama pri každom renderi
-
-
-  useEffect (() => {
-    fetch('data.json') // bez bodky a bez lomky na zaciatku
+// ========================================== 
+// vkladanie obrazkov zo suboru data.json / nacitavanie dat
+useEffect (() => {
+  fetch('data.json')
     .then(response => response.json())
     .then(data => {
       setProducts(data);
       if (data.length && data[0].image.length > 0) {
         setProductImages(data[0].image[0].picture);
+        console.log("Dáta sa načítali:", data);
 
-        // tento kod prednacita obrazky
+        // prednacitanie obrazkov
         data[0].image.forEach((item) => {
           const img = new Image();
           img.src = item.picture;
         })
       }
     })
-    .catch(error => console.error ("Chyba:", error));
+    .catch(error => console.error ("Chyba:", error));      
 }, []);
+// ==========================================
 
-  return (
-    <>
-        <div className="main__container">
-          <header className="navigation__panel">
-            <img src={Close} alt="close" aria-hidden="true" />
+// ******************************************
+// Vykreslovanie
+return (
+  <>
+  {/* Ak products ešte nie sú načítané, ukážeme len načítavanie a nepustíme kód ďalej */}
+    {products.length === 0 ? (
+      <div>Načítavam produkty...</div>
+    ) : (
+    <div className="main__container">
 
-            <button type="button">
-              <img src={Menu} alt="Menu" aria-hidden="true" />
-            </button>
+      {/* // ========================================== */}
+      {/* navigacny panel */}      
+      <header className="navigation__panel"> 
 
-            <img src={Logo} alt="Logo" aria-hidden="true" />
+        {/* // ---------------------------------------- */}
+        {/* nastavenie vyskakovacieho menu */}
+        <button 
+          type="button"
+          className="menu__button"
+          aria-hidden="true"
+          onClick={() => 
+            setMenu(true)
+          }>
+          <img src={Menu} alt="Menu" aria-hidden="true" />
+        </button>    
 
-            <nav>
-              <ul>
-                <li>Collections</li>
-                <li>Men</li>
-                <li>Women</li>
-                <li>About</li>
-                <li>Contact</li>
-              </ul>
-            </nav>
-            
-            <button type="button" aria-hidden="true">
-              <img src={Cart} alt="Cart" aria-hidden="true" />
-            </button>
-
-            <button type="button" aria-hidden="true">
-              <img src={Avatar} alt="Avatar" aria-hidden="true" />
-            </button>
-          </header>
-
-          <article className="product__page">
-
-            {/* // ========================================== */}
-            {/*sem sa budu vkladat obrazky*/}
-            {/* // ========================================== */}
-            <div className="main__image">
-              <img src={productImages} alt="Main product" />
-            </div>
-
-            {products[0]?.image.map((item, index) => (
-              <div 
-                key={index}
-                className="thumbnail-wrapper"
-                onClick={() => {
-                  console.log("Klik na obrazok:", item.image.picture);
-                  setProductImages(item.image.picture);
-                }}>
-                
-              <img 
-                key={index} 
-                src={item.image.thumbnail} 
-                alt={item.name} 
-                className={productImages === item.image.picture ? "thumbnail--active" : "thummbnail"}
-                onClick={() => setProductImages(item.image.picture)}/>
-              </div>
-            ))}
-
-            
-            
-            <p>SNEAKER COMPANY</p>
-
-            <h1>Fall Limited Edition Sneakers</h1>
-
-            <p>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they&#39;ll withstand everything the weather can offer.</p>
-
-            <div> 
-              <p>{unitPrice.toFixed(2)}</p>
-              <p>{products[0]?.price}</p>
-              <p>{products[0]?.originalPrice?.toFixed(2)}</p>
-            </div>
-
-            {/* // ========================================== */}
-            {/* sem pride +/- pre pridanie a odoberanie z kosika */}
-            {/* // ========================================== */}
-            <div className="quantity__control"> {/* Obalovací prvok pre celé ovládanie množstva (mínus, číslo, plus)*/}
-              {/* Tlačidlo pre zníženie množstva */}
-              <button 
-                type="button" /* Definuje, že ide o obyčajné tlačidlo (neodosiela formulár) */
-                onClick={() => setQuantity(prev => prev > 0 ? prev - 1 : 0)}> {/*onClick funkcia: prev je aktuálna hodnota (previous state). */}
-                                                                              {/* Používame podmienku: Ak je prev viac ako 0, odpočítaj 1. */}
-                                                                              {/* Ak už je 0, nechaj tam 0 (vďaka tomu nepôjdeš do mínusu). */}
-              <img src={Minus} alt="" aria-hidden="true" />
-              </button>
-
-              <span>{quantity}</span> {/* Dynamické zobrazenie aktuálneho čísla zo stavu 'quantity' */}
-
-              {/* // ========================================== */}
-              {/* Tlačidlo pre zvýšenie množstva */}
-              {/* // ========================================== */}
-              <button
-                type="button"
-                onClick={() => setQuantity(prev => prev + 1)} > 
-                <img src={Plus} alt="" aria-hidden="true" />
-              </button>
-            </div>
-
-            {/* // ========================================== */}
-            {/* Tlačidlo na pridanie do košíka */}
-            {/* // ========================================== */}
-            <button 
+        {/* Ak je premenná menu pravdivá (true), vykresli to, čo je v zátvorke. Ak je false, v prehliadači sa nevykreslí vôbec nič */}
+        {menu &&  
+          <nav className="menu__list">
+            <button
               type="button"
-              onClick={() => quantity > 0 && setAddCart(true)}> 
-              {/* Logická podmienka: setAddCart(true) sa spustí IBA vtedy, ak je quantity (množstvo) väčšie ako nula. Ak je tam 0, na kliknutie to nereaguje (prázdny produkt nepridáš) */}
-              <img src={Cart} alt="Cart" aria-hidden="true" />Add to cart
+              className="close__button"
+              aria-hidden="true"
+              onClick={() => setMenu(false)}>
+                <img src={Close} alt="close" aria-hidden="true" />
             </button>
+            <ul>
+              <li>Collections</li>
+              <li>Men</li>
+              <li>Women</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        }
+        {/* // ------------------------------------------ */}
 
-          </article>
+        <img src={Logo} alt="Logo" aria-hidden="true" />
 
-            {/* // ========================================== */}
-            {/* po vybere produktu sa nad obrazkom zobrazi "Empty Cart"*/}
-            {/* // ========================================== */}
-            <div> {/* kosik */}
-              <h3>Cart</h3>
+        {/* // _________________________________________ */}
+        {/* vstup do kosika cez ikonku */}
+        <button type="button"
+              className="close__button"
+              aria-hidden="true"
+              onClick={() => setCart(!cart)}>
+          <img src={Cart} alt="Cart" aria-hidden="true" />
 
-              <div> {/* prazdny kosik */}
-                <p> Your cart is empty</p>
-              </div>
+          {/* Ak je množstvo väčšie ako nula, ukáž to, čo je v zátvorke.*/}
+          {addCart && quantity > 0 && ( 
+            <span className="cart__quantity-badge">{quantity}</span>
+          )}
+        </button>
+        {/* // __________________________________________ */}
 
-              <div> {/* kosik po vybere produktu */}
-                <div>
-                  {/* nahladovy maly obrazok */}
-                  <img src={products[0]?.image.thumbnail} alt="product thumbnail" />
-                  <p>Fall Limited Edition Sneakers</p>
-                  <div>
-                    {/* jednotkova cena */} <p>x</p> {/* mnozstvo */} {/* cena celkom */} 
-                    <p>${unitPrice.toFixed(2)} x {quantity} ${totalPrice.toFixed(2)}</p>
-                  </div>
-                  <img 
-                    src={Bascket} 
-                    alt="Bascket" 
-                    aria-hidden="true"
-                    onClick={() => {
-                      setAddCart(false); // skryje pprodukt z kosika
-                      setQuantity(0); // Vynuluje počítadlo
-                    }}/>
-                </div>
+        <button type="button" aria-hidden="true">
+          <img src={Avatar} alt="Avatar" aria-hidden="true" />
+        </button>        
+      </header>
+      {/* // =============================================== */}
 
-                <button type="button">Checkout</button>
-              </div>
+      <article className="product__container">
+
+        {/* // ========================================== */}
+        {/* priestor pre obrazky */}
+        <div className="image__container">
+
+          {/* // ----------------------------------------- */}
+          {/* hlavny obrazok */}
+          <div className="main__image">
+            <img src={productImages} alt="Main product" />
+          </div>
+          {/* // ------------------------------------------ */}
+
+          {/* // __________________________________________ */}
+          {/* thumbnails obrazky pod hlavnym obrazkom */}
+          {products[0]?.image.map((item, index) => (
+            <div
+              key={index}
+              className="thumbnail__image">
+
+              <img
+                src={item.thumbnail}
+                alt={item.name}
+                className={productImages === item.picture ? "thumbnail--active" : "thummbnail"}
+                onClick={() => setProductImages(item.picture)}/>
             </div>
+            ))}
+          {/* // ________________________________________ */}
+        </div>
+        {/* // ========================================== */}
+
+        {/* // ========================================== */}
+        {/* informacie o produkte a cena */}
+        <div className="product__info-container">
+          <div className="product__info">
+            <p className="product__info-company">{products[0]?.company}</p>
+            <h1 className="product__info-name">{products[0]?.name}</h1>
+            <p className="product__info-description">{products[0]?.description}</p>
+          </div>
+
+          <div className="product__price">
+            <p className="product__price-unit">{products[0]?.Price > 0 ? products[0]?.Price.toFixed(2) : "0.00"}</p>
+            <p className="product__price-discoount">{products[0]?.discount}</p>
+            <p className="product__price-original">{products[0]?.originalPrice}</p>
+          </div>
+        </div>
+        {/* // ========================================== */}
+
+        {/* // ========================================== */}
+        {/* +/- pre pridanie a odoberanie z kosika */}
+        <div className="quantity__control">
+          {/* // ________________________________________ */}
+          {/* Tlačidlo pre zníženie množstva */}
+          <button
+            type="button"
+            aria-hidden="true"
+            onClick={() => setQuantity(prev => prev > 0 ? prev - 1 : 0)}>
+              {/*onClick funkcia: prev je aktuálna hodnota (previous state). Používame podmienku: Ak je prev viac ako 0, odpočítaj 1. Ak už je 0, nechaj tam 0, vďaka tomu nepôjde do mínusu). */}
+            <img src={Minus} alt="" aria-hidden="true" />
+          </button>
+          {/* // ________________________________________ */}
+
+          {/* ############################################ */}
+          {/* Dynamické zobrazenie aktuálneho čísla zo stavu 'quantity' */}
+          <span className="quantity">{quantity}</span>
+          {/* ############################################ */}
+
+          {/* // ---------------------------------------- */}
+          {/* Tlačidlo pre zvysenie množstva */}
+          <button
+            type="button"
+            aria-hidden="true"
+            onClick={() => setQuantity(prev => prev + 1)}>
+            <img src={Plus} alt="" aria-hidden="true" />
+          </button>
+          {/* // ---------------------------------------- */}
+
+          {/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */}
+          {/* Tlačidlo na pridanie do košíka */}
+          <button
+            type="button"
+            disabled={quantity === 0} // Tlačidlo sa nedá stlačiť, ak je množstvo 0
+            onClick={() => setAddCart(true)}>
+              <img src={Cart} alt="Cart" aria-hidden="true" /> 
+              <span className="add__to-cart">Add to cart</span>
+          </button>
+          {/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */}
+
 
         </div>
+        {/* // ========================================== */}
+      </article>
 
-        <footer className="attribution">
+      <section className="cart__container"></section>
+    </div>
+    )}
+
+    <footer className="attribution">
         Challenge by
         <a href="https://www.frontendmentor.io?ref=challenge" className="attribution-link" target="_blank" rel="noopener noreferrer">
           Frontend Mentor</a>
@@ -193,9 +218,10 @@ const CommerceProduct = () => { // idem vyberat produkty a vkladat ich cez butto
           Adriana Weidlichova</a>
         .
       </footer> 
-    </>
-  );
+    
+  </>
+);
 };
-
+// *********************************************
 
 export default CommerceProduct;
