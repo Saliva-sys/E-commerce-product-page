75961c2ba46eb30fbe910dd3cb4e4e09e901440c
Saliva-sys <aweidlichova@gmail.com>
2026-03-31 12:29:10 +0200

    docs: update project data structure

--- src/CommerceProduct.jsx
+++ src/CommerceProduct.jsx
@@ -6,14 +6,20 @@ import Cart from "./assets/icon-cart.svg";
 import Bascket from "./assets/icon-delete.svg";
 import Close from "./assets/icon-close.svg";
 import Avatar from "./assets/image-avatar.png";
+import Plus from "./assets/icon-plus.svg";
+import Minus from "./assets/icon-minus.svg";
 
 
 const CommerceProduct = () => { // idem vyberat produkty a vkladat ich cez button "Add to Cart" do kosika
   const [products, setProducts] = useState([]);
   const [addCart, setAddCart] = useState(false);
-
+  const [quantity, setQuantity] = useState(1);
+  
   // idem vyberat z thumbnails obrazky
   const [productImages,setProductImages] = useState();
+
+  const unitPrice = products[0]?.price || 0; // Cena za jeden kus
+  const totalPrice = unitPrice * quantity; // Celková cena sa vypočíta sama pri každom renderi
 
 
   useEffect (() => {
@@ -21,8 +27,14 @@ const CommerceProduct = () => { // idem vyberat produkty a vkladat ich cez butto
     .then(response => response.json())
     .then(data => {
       setProducts(data);
-      if (data.length > 0) {
-        setProductImages(data[0].image.picture);
+      if (data.length && data[0].image.length > 0) {
+        setProductImages(data[0].image[0].picture);
+
+        // tento kod prednacita obrazky
+        data[0].image.forEach((item) => {
+          const img = new Image();
+          img.src = item.picture;
+        })
       }
     })
     .catch(error => console.error ("Chyba:", error));
@@ -61,19 +73,31 @@ const CommerceProduct = () => { // idem vyberat produkty a vkladat ich cez butto
 
           <article className="product__page">
 
+            {/* // ========================================== */}
             {/*sem sa budu vkladat obrazky*/}
+            {/* // ========================================== */}
             <div className="main__image">
               <img src={productImages} alt="Main product" />
             </div>
 
-            {products.map((item, index) => (
+            {products[0]?.image.map((item, index) => (
+              <div 
+                key={index}
+                className="thumbnail-wrapper"
+                onClick={() => {
+                  console.log("Klik na obrazok:", item.image.picture);
+                  setProductImages(item.image.picture);
+                }}>
+                
               <img 
                 key={index} 
                 src={item.image.thumbnail} 
                 alt={item.name} 
                 className={productImages === item.image.picture ? "thumbnail--active" : "thummbnail"}
                 onClick={() => setProductImages(item.image.picture)}/>
+              </div>
             ))}
+
             
             
             <p>SNEAKER COMPANY</p>
@@ -83,17 +107,51 @@ const CommerceProduct = () => { // idem vyberat produkty a vkladat ich cez butto
             <p>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they&#39;ll withstand everything the weather can offer.</p>
 
             <div> 
-              <h2>$125.00</h2>
-              <p>50%</p>
-              <p>$250.00</p>
+              <p>{unitPrice.toFixed(2)}</p>
+              <p>{products[0]?.price}</p>
+              <p>{products[0]?.originalPrice?.toFixed(2)}</p>
             </div>
 
+            {/* // ========================================== */}
             {/* sem pride +/- pre pridanie a odoberanie z kosika */}
-            
-            <button type="button"> <img src={Cart} alt="Cart" aria-hidden="true" />Add to cart</button>
+            {/* // ========================================== */}
+            <div className="quantity__control"> {/* Obalovací prvok pre celé ovládanie množstva (mínus, číslo, plus)*/}
+              {/* Tlačidlo pre zníženie množstva */}
+              <button 
+                type="button" /* Definuje, že ide o obyčajné tlačidlo (neodosiela formulár) */
+                onClick={() => setQuantity(prev => prev > 0 ? prev - 1 : 0)}> {/*onClick funkcia: prev je aktuálna hodnota (previous state). */}
+                                                                              {/* Používame podmienku: Ak je prev viac ako 0, odpočítaj 1. */}
+                                                                              {/* Ak už je 0, nechaj tam 0 (vďaka tomu nepôjdeš do mínusu). */}
+              <img src={Minus} alt="" aria-hidden="true" />
+              </button>
+
+              <span>{quantity}</span> {/* Dynamické zobrazenie aktuálneho čísla zo stavu 'quantity' */}
+
+              {/* // ========================================== */}
+              {/* Tlačidlo pre zvýšenie množstva */}
+              {/* // ========================================== */}
+              <button
+                type="button"
+                onClick={() => setQuantity(prev => prev + 1)} > 
+                <img src={Plus} alt="" aria-hidden="true" />
+              </button>
+            </div>
+
+            {/* // ========================================== */}
+            {/* Tlačidlo na pridanie do košíka */}
+            {/* // ========================================== */}
+            <button 
+              type="button"
+              onClick={() => quantity > 0 && setAddCart(true)}> 
+              {/* Logická podmienka: setAddCart(true) sa spustí IBA vtedy, ak je quantity (množstvo) väčšie ako nula. Ak je tam 0, na kliknutie to nereaguje (prázdny produkt nepridáš) */}
+              <img src={Cart} alt="Cart" aria-hidden="true" />Add to cart
+            </button>
+
           </article>
 
+            {/* // ========================================== */}
             {/* po vybere produktu sa nad obrazkom zobrazi "Empty Cart"*/}
+            {/* // ========================================== */}
             <div> {/* kosik */}
               <h3>Cart</h3>
 
@@ -104,11 +162,20 @@ const CommerceProduct = () => { // idem vyberat produkty a vkladat ich cez butto
               <div> {/* kosik po vybere produktu */}
                 <div>
                   {/* nahladovy maly obrazok */}
-                  {/* nazov produktu */}
+                  <img src={products[0]?.image.thumbnail} alt="product thumbnail" />
+                  <p>Fall Limited Edition Sneakers</p>
                   <div>
-                    {/* jednotkova cena */} <p>x</p> {/* mnozstvo */} {/* cenea celkom */} 
+                    {/* jednotkova cena */} <p>x</p> {/* mnozstvo */} {/* cena celkom */} 
+                    <p>${unitPrice.toFixed(2)} x {quantity} ${totalPrice.toFixed(2)}</p>
                   </div>
-                  <img src={Bascket} alt="Bascket" aria-hidden="true" />
+                  <img 
+                    src={Bascket} 
+                    alt="Bascket" 
+                    aria-hidden="true"
+                    onClick={() => {
+                      setAddCart(false); // skryje pprodukt z kosika
+                      setQuantity(0); // Vynuluje počítadlo
+                    }}/>
                 </div>
 
                 <button type="button">Checkout</button>
