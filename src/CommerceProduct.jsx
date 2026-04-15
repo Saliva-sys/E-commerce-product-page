cdb05d77ee9f0e8f9565c867721c98f929812f91
Saliva-sys <aweidlichova@gmail.com>
2026-04-15 17:42:30 +0200

    feat: improve cart positioning and fix accessibility issues

--- src/CommerceProduct.jsx
+++ src/CommerceProduct.jsx
@@ -130,11 +130,103 @@ return (
               )}
             </button>
             {/* // __________________________________________ */}
+
+            {/* ///////////////////////////////////////////// */}
+          {/* Definujeme zobrazenie jednotlivych stavov kosika */}
+          {isCartOpen && (
+          // ==========================================
+          // Nastavenie kosika         
+          <section className="cart__container">
+            <div className="cart__shadow-layer"></div>
+
+            <div className="cart__content-layer">
+              <h3 className="cart__container-title">Cart</h3>
+
+            <div className="cart__content">
+              {!addCart || cartQuantity ===0 ? (
+
+                // -------------------------------------------- 
+                // Empty cart
+                <div className="empty__cart">
+                  <p className="empty__cart-text">Your cart is empty.</p>
+                </div>
+                // -------------------------------------------- 
+              
+          ) : (
+                // ***********************************************
+                // kosik po vybere produktu 
+                <div className="product__list">
+                  <div className="product__list-item">
+                    <img 
+                      src={products[0]?.image[0]?.thumbnail} 
+                      className="cart__product-image" 
+                      alt="product thumbnail" />
+
+                    <div className="product__list-info">
+                      <p className="cart__product-name">{products[0]?.name}</p>
+                    
+
+                      <div className="cart__product-price">
+                        <p>
+                          <span className="cart__product-price-unit">{products[0]?.price}</span> 
+                          <span className="cart__product-price-multiple">x</span> 
+                          <span className="cart__product-price-quantity">{cartQuantity}</span> 
+                          <span className="cart__product-price-total">{ (products[0]?.price * cartQuantity).toFixed(2) }</span>
+                        </p>
+                      </div>
+                    </div>
+
+                    {/* ______________________________________________ */}
+                    {/* Vyprazdnenie kosika */}
+                    <img 
+                      src={Basket} 
+                      className="cart__product-delete"
+                      alt="Basket" 
+                      aria-hidden="true"
+                      onClick={() => {
+                        setAddCart(false); // skryje pprodukt z kosika
+                        setQuantity(0); // Vynuluje počítadlo
+                    }}/>
+                    {/* ______________________________________________ */}
+                  </div>
+                  
+                  <div>
+                    <button 
+                      className="checkout__button" 
+                      type="button" 
+                      onClick={() => {
+                        Swal.fire({
+                          title: 'Thank you!',
+                          text: 'Your fictive purchase was successful.',
+                          icon: 'success',
+                          confirmButtonText: 'Great!',
+                          confirmButtonColor: 'var(--color-orange)', 
+                          background: 'var(--color-very-dark-blue)',
+                          color: 'var(--color-light-orange)',
+                          borderRadius: '10px'
+                        });
+                      }}
+                    >
+                      Checkout
+                    </button>
+                  </div>
+                  {/* *********************************************** */}
+                </div>
+              )}
+            </div>     
+          </div>                
+        </section>
+      )}
           </div>
 
-          <button className="avatar__button" type="button" aria-label="Profile">
-            <a href="https://github.com/Saliva-sys" target="blank"><img src={Avatar} className="avatar__button-img" alt="Avatar" aria-hidden="true" /></a>
-          </button>   
+          <span className="avatar__button">
+            <a href="https://github.com/Saliva-sys" target="_blank" rel="noreferrer" aria-label="Visit Adriana's GitHub profile">
+            <img src={Avatar} className="avatar__button-img" alt="" aria-hidden="true" />
+            </a>
+          </span>
+
+          
+      {/* ///////////////////////////////////////////// */}    
         </div>     
       </header>
       {/* // =============================================== */}
@@ -262,7 +354,7 @@ return (
               <button
                 type="button"
                 className="decrement__button"
-                aria-hidden="true"
+                aria-label="Decrement"
                 onClick={() => setQuantity(prev => prev > 0 ? prev - 1 : 0)}>
                   {/*onClick funkcia: prev je aktuálna hodnota (previous state). Používame podmienku: Ak je prev viac ako 0, odpočítaj 1. Ak už je 0, nechaj tam 0, vďaka tomu nepôjde do mínusu). */}
                 <img src={Minus} className="decrement__button-img" alt="" aria-hidden="true" />
@@ -279,7 +371,7 @@ return (
               <button
                 type="button"
                 className="increment__button"
-                aria-hidden="true"
+                aria-label="Increment"
                 onClick={() => setQuantity(prev => prev + 1)}>
                 <img src={Plus} className="increment__button-img" alt="" aria-hidden="true" />
               </button>
@@ -308,93 +400,7 @@ return (
         </div>
       </article>
 
-      {/* ///////////////////////////////////////////// */}
-      {/* Definujeme zobrazenie jednotlivych stavov kosika */}
-      {isCartOpen && (
-        // ==========================================
-        // Nastavenie kosika         
-        <section className="cart__container">
-          <div className="cart__shadow-layer"></div>
-
-          <div className="cart__content-layer">
-            <h3 className="cart__container-title">Cart</h3>
-
-            <div className="cart__content">
-              {!addCart || cartQuantity ===0 ? (
-
-                // -------------------------------------------- 
-                // Empty cart
-                <div className="empty__cart">
-                  <p className="empty__cart-text">Your cart is empty.</p>
-                </div>
-                // -------------------------------------------- 
-              
-        ) : (
-                // ***********************************************
-                // kosik po vybere produktu 
-                <div className="product__list">
-                  <div className="product__list-item">
-                    <img 
-                      src={products[0]?.image[0]?.thumbnail} 
-                      className="cart__product-image" 
-                      alt="product thumbnail" />
-
-                    <div className="product__list-info">
-                      <p className="cart__product-name">{products[0]?.name}</p>
-                    
-
-                      <div className="cart__product-price">
-                        <p>
-                          <span className="cart__product-price-unit">{products[0]?.price}</span> 
-                          <span className="cart__product-price-multiple">x</span> 
-                          <span className="cart__product-price-quantity">{cartQuantity}</span> 
-                          <span className="cart__product-price-total">{ (products[0]?.price * cartQuantity).toFixed(2) }</span>
-                        </p>
-                      </div>
-                    </div>
-
-                    {/* ______________________________________________ */}
-                    {/* Vyprazdnenie kosika */}
-                    <img 
-                      src={Basket} 
-                      className="cart__product-delete"
-                      alt="Basket" 
-                      aria-hidden="true"
-                      onClick={() => {
-                        setAddCart(false); // skryje pprodukt z kosika
-                        setQuantity(0); // Vynuluje počítadlo
-                    }}/>
-                    {/* ______________________________________________ */}
-                  </div>
-                  
-                  <div>
-                    <button 
-                      className="checkout__button" 
-                      type="button" 
-                      onClick={() => {
-                        Swal.fire({
-                          title: 'Thank you!',
-                          text: 'Your fictive purchase was successful.',
-                          icon: 'success',
-                          confirmButtonText: 'Great!',
-                          confirmButtonColor: 'var(--color-orange)', 
-                          background: 'var(--color-very-dark-blue)',
-                          color: 'var(--color-light-orange)',
-                          borderRadius: '10px'
-                        });
-                      }}
-                    >
-                      Checkout
-                    </button>
-                  </div>
-                  {/* *********************************************** */}
-                </div>
-              )}
-            </div>     
-          </div>                
-        </section>
-      )}
-      {/* ///////////////////////////////////////////// */}            
+                
     </div>
     )}
 
